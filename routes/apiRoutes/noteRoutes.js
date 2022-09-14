e1f40601e86a4674d30f934b81a97ac3e3378d82
Jorge Garit <jorgegarit@gmail.com>
2022-09-13 23:04:56 -0400

    added the route.het request

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,3 +4,16 @@ const router = require('express').Router();
 const { notes } = require('../../db/db');
 const { createNotes } = require('../../lib/note');
 
+// router to get notes from note.json
+router.get('/notes', (req, res) => {
+    let savedNotes = notes;
+
+    // addind saved notes to front end 
+    res.json(savedNotes);
+});
+
+//  router to post new notes to front end 
+router.post('/notes', (req, res) => {
+    // set id based on what the next index in the notesArray will be 
+    req.body.id = notes.length.toString();
+})
