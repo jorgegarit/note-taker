7047e0521d11d384d5729676fad50fd607e8bc60
Jorge Garit <jorgegarit@gmail.com>
2022-09-13 23:59:38 -0400

    reattempting

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const noteRoutes = require('./noteRoutes'); // since the lie in the same folder ./ works fine
 
 router.use(noteRoutes);
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 // calling the json file where notes will be stored and note.js for functions
-const { notes } = require('../../db/db');
+const { notes } = require('../../db/db.json');
 const { createNotes } = require('../../lib/note');
 
 // api routes
