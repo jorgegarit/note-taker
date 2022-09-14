c9da437f23cc0040495ec987cf0ab92c8aa7c408
Jorge Garit <jorgegarit@gmail.com>
2022-09-14 00:39:42 -0400

    finally got it to work it was not letting me save notes due to issue locating length on notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const notes = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
-const noteRoutes = require('./noteRoutes'); // since the lie in the same folder ./ works fine
+const createNotes = require('../../lib/note');
 
-router.use(noteRoutes);
+// api routes
+// router to get notes from note.json
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+//  router to post new notes to front end and save notes
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString()
+    const newNote = createNotes(req.body, notes)
+    res.json(newNote);
+});
 
 module.exports = router;
